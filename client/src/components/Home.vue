47fe0c145cb327e4da66daf06650fe5bf41ecf5c
Andre <andriansimamora@gmail.com>
2018-09-07 13:55:58 +0700

    [update] - replace jumbotron with card

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,13 +1,11 @@
 <template>
-<v-jumbotron color="grey lighter-3">
-  <v-container fill-height>
-    <v-layout align-center>
-      <v-flex>
-        <h1 class="display-3 text-xs-center">Welcome to Duhansgram Dashboard</h1>
-      </v-flex>
-    </v-layout>
-  </v-container>
-</v-jumbotron>
+<v-container>
+  <v-card>
+    <v-card-text>
+      <h1 class="display-3 text-xs-center">Welcome to Duhansgram Dashboard</h1>
+    </v-card-text>
+  </v-card>
+</v-container>
 </template>
 
 <script>
