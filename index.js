481d4c9f8464e87b2252051d19ff1dbc46a76a16
Andre <andriansimamora@gmail.com>
2018-09-07 14:17:34 +0700

    [update] change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path    = require('path');
 
-const PORT = 8070;
+const PORT = process.env.PORT || 8070;
 const app = express();
 
 app.use(express.static('client/dist'));
@@ -10,4 +10,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client/dist/index.html'));
 });
 
-app.listen(PORT, () => console.log(`App listenin on port ${8070}`));
+app.listen(PORT, () => console.log(`App listenin on port ${PORT}`));
